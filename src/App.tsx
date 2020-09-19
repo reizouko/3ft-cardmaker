cab39857c177cc35b4ed5ed9174762355d7985b5
reizouko <reizouko.info@gmail.com>
2020-09-19 20:01:45 +0900

    material-uiを導入。完成図を表示するようにした。

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 import html2canvas from 'html2canvas';
+import { Button, Container, FormControl, FormControlLabel, FormLabel, Grid, makeStyles, Radio, RadioGroup, TextField } from '@material-ui/core';
+import QRCode from "qrcode.react";
 
 const cardSize = {
   width: 744,
@@ -23,6 +25,20 @@ const imageSizeButtons = [
   {value: "contain", label: "カードに収まるように配置する"},
 ]
 
+const useStyles = makeStyles({
+  cardPart: {
+    position: "absolute",
+    display: "inline-block",
+  },
+  cardText: {
+    position: "absolute",
+    display: "inline-block",
+    fontFamily: "'Noto Sans JP',sans-serif",
+    color: "#ffffff",
+    textShadow: "2px 2px 1px #000000, -2px 2px 1px #000000, 2px -2px 1px #000000, -2px -2px 1px #000000, 2px 0px 1px #000000, 0px 2px 1px #000000, -2px 0px 1px #000000, 0px -2px 1px #000000",
+  }
+});
+
 const App: React.FC = () => {
   const [rarity, setRarity] = useState(rarityButtons[0].value);
   const [attribute, setAttribute] = useState(attributeButtons[0].value);
@@ -33,6 +49,8 @@ const App: React.FC = () => {
   const [ability, setAbility] = useState("能力名：なんかすごい能力");
   const [abilityNote, setAbilityNote] = useState("能力の説明をここに入れるといいかもしれません。");
   const [description, setDescription] = useState("ここに説明テキストを入れてね。");
+  const [qrText, setQrText] = useState("");
+  const [cardData, setCardData] = useState("");
 
   const fileElement = useRef<HTMLInputElement>(null);
   const previewElement = useRef<HTMLDivElement>(null);
@@ -50,160 +68,139 @@ const App: React.FC = () => {
     }
   };
 
-  const download = () => {
+  const generate = () => {
     html2canvas(previewElement.current as HTMLElement, {
-      x: 0,
-      y: 0,
       width: cardSize.width,
       height: cardSize.height
     }).then(canvas => {
-      const downloadLink = document.createElement("a");
-      downloadLink.href = canvas.toDataURL();
-      downloadLink.download = "card.png";
-      downloadLink.click();
+      setCardData(canvas.toDataURL());
     });
   };
 
-  return <>
-    <div id="cardpreview" ref={previewElement} style={{position: "relative", width: `${cardSize.width}px`, height: `${cardSize.height}px`, margin: "20px"}}>
-      <div style={{
-        backgroundImage: cardImage,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: imageSize,
-        position: "absolute", left: "0", top: "0",
-        width: `${cardSize.width}px`, height: `${cardSize.height}px`,
-        zIndex: 0
-      }}></div>
-      <img
-        src={`${process.env.PUBLIC_URL}/cardfront/${rarity}_${attribute}.png`}
-        width={cardSize.width}
-        height={cardSize.height}
-        style={{
-          position: "absolute",
-          left: "0", 
-          top: "0",
-          zIndex: 1
-        }}
-      />
-      <div id="name" style={{
-        position: "absolute",
-        display: "inline-block",
-        left: "150px",
-        top: "705px",
-        color: "#ffffff",
-        font: "bold 40px メイリオ",
-        letterSpacing: "3px",
-        textShadow: "2px 2px 1px #000000, -2px 2px 1px #000000, 2px -2px 1px #000000, -2px -2px 1px #000000, 2px 0px 1px #000000, 0px 2px 1px #000000, -2px 0px 1px #000000, 0px -2px 1px #000000",
-        zIndex: 2
-      }}>{name}</div>
-      <div id="title" style={{
-        position: "absolute",
-        display: "inline-block",
-        right: "75px",
-        top: "725px",
-        color: "#ffffff",
-        font: "bold 22px メイリオ",
-        letterSpacing: "3px",
-        textShadow: "2px 2px 1px #000000, -2px 2px 1px #000000, 2px -2px 1px #000000, -2px -2px 1px #000000, 2px 0px 1px #000000, 0px 2px 1px #000000, -2px 0px 1px #000000, 0px -2px 1px #000000",
-        zIndex: 3
-      }}>{title}</div>
-      <div id="ability" style={{
-        position: "absolute",
-        display: "block",
-        left: "50%",
-        top: "800px",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-        color: "#ffffff",
-        font: "normal 22px メイリオ",
-        letterSpacing: "3px",
-        textShadow: "2px 2px 1px #000000, -2px 2px 1px #000000, 2px -2px 1px #000000, -2px -2px 1px #000000, 2px 0px 1px #000000, 0px 2px 1px #000000, -2px 0px 1px #000000, 0px -2px 1px #000000",
-        zIndex: 4
-      }}>{ability}</div>
-      <div id="abilityNote" style={{
-        position: "absolute",
-        display: "inline-block",
-        left: "40px",
-        top: "825px",
-        maxWidth: "660px",
-        color: "#ffffff",
-        font: "normal 14px メイリオ",
-        letterSpacing: "1px",
-        textShadow: "2px 2px 1px #000000, -2px 2px 1px #000000, 2px -2px 1px #000000, -2px -2px 1px #000000, 2px 0px 1px #000000, 0px 2px 1px #000000, -2px 0px 1px #000000, 0px -2px 1px #000000",
-        zIndex: 5
-      }}>{abilityNote}</div>
-      <div id="description" style={{
-        position: "absolute",
-        display: "inline-block",
-        left: "40px",
-        top: "875px",
-        maxWidth: "660px",
-        color: "#ffffff",
-        font: "normal 18px メイリオ",
-        letterSpacing: "1px",
-        lineHeight: "32px",
-        whiteSpace: "pre-wrap",
-        textShadow: "2px 2px 1px #000000, -2px 2px 1px #000000, 2px -2px 1px #000000, -2px -2px 1px #000000, 2px 0px 1px #000000, 0px 2px 1px #000000, -2px 0px 1px #000000, 0px -2px 1px #000000",
-        zIndex: 5
-      }}>{description}</div>
-    </div>
-    <div id="parameters">
-      <form>
+  const classes = useStyles();
+
+  return <Container fixed>
+    <Grid container spacing={2}>
+      <Grid item>
+        <div ref={previewElement} style={{position: "relative", width: `${cardSize.width}px`, height: `${cardSize.height}px`}}>
+          <div className={classes.cardPart} style={{
+            backgroundImage: cardImage,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: imageSize,
+            left: "0", top: "0",
+            width: `${cardSize.width}px`, height: `${cardSize.height}px`,
+            zIndex: 0
+          }}></div>
+          <img
+            src={`${process.env.PUBLIC_URL}/cardfront/${rarity}_${attribute}.png`}
+            width={cardSize.width}
+            height={cardSize.height}
+            className={classes.cardPart}
+            style={{
+              left: "0", 
+              top: "0",
+              zIndex: 1
+            }}
+          />
+          <div id="name" className={classes.cardText} style={{
+            left: "152px",
+            top: "703px",
+            fontWeight: "bold",
+            fontSize: "40px",
+            letterSpacing: "3px",
+            zIndex: 2
+          }}>{name}</div>
+          <div id="title" className={classes.cardText} style={{
+            right: "70px",
+            top: "723px",
+            fontWeight: 500,
+            fontSize: "24px",
+            fontStyle: "oblique",
+            letterSpacing: "3px",
+            zIndex: 3
+          }}>{title}</div>
+          <div id="ability" className={classes.cardText} style={{
+            left: "50%",
+            top: "800px",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            fontSize: "22px",
+            letterSpacing: "3px",
+            zIndex: 4
+          }}>{ability}</div>
+          <div id="abilityNote" className={classes.cardText} style={{
+            left: "40px",
+            top: "825px",
+            maxWidth: "660px",
+            fontSize: "14px",
+            letterSpacing: "1px",
+            zIndex: 5
+          }}>{abilityNote}</div>
+          <div id="description" className={classes.cardText} style={{
+            left: "40px",
+            top: "875px",
+            maxWidth: "660px",
+            fontSize: "18px",
+            letterSpacing: "1px",
+            lineHeight: "32px",
+            whiteSpace: "pre-wrap",
+            zIndex: 6
+          }}>{description}</div>
+          { qrText.length > 0 && <QRCode value={qrText} size={62} className={classes.cardPart} style={{
+            left: "640px",
+            top: "47px",
+            zIndex: 7
+          }}/> }
+        </div>
+      </Grid>
+      <Grid item>
         <div>
-          <div>画像ファイル</div>
-          <div><input type="file" accept="image/*" ref={fileElement} onChange={fileSelected}/></div>
-          {
-            imageSizeButtons.map(data => <label key={`size_${data.value}`}>
-              <input type="radio" name="imagesize" value={data.value} checked={imageSize === data.value} onChange={() => setImageSize(data.value)}/>
-              {data.label}
-            </label>)
-          }
+          <FormControl component="fieldset">
+            <FormLabel component="legend">画像</FormLabel>
+            <div><input type="file" accept="image/*" ref={fileElement} onChange={fileSelected}/></div>
+            <RadioGroup aria-label="imagesize" name="imagesize" value={imageSize} onChange={e => setImageSize(e.target.value)}>
+            {
+              imageSizeButtons.map(data => <FormControlLabel key={`imagesize_${data.value}`} value={data.value} control={<Radio/>} label={data.label}/>)
+            }
+            </RadioGroup>
+          </FormControl>
         </div>
         <div>
-          <div>レアリティ</div>
-          {
-            rarityButtons.map(data => <label key={`rarity_${data.value}`}>
-              <input type="radio" name="rarity" value={data.value} checked={rarity === data.value} onChange={() => setRarity(data.value)}/>
-              {data.label}
-            </label>)
-          }
+          <FormControl component="fieldset">
+            <FormLabel component="legend">レアリティ</FormLabel>
+            <RadioGroup aria-label="rarity" name="rarity" value={rarity} onChange={e => setRarity(e.target.value)}>
+            {
+              rarityButtons.map(data => <FormControlLabel key={`rarity_${data.value}`} value={data.value} control={<Radio/>} label={data.label}/>)
+            }
+            </RadioGroup>
+          </FormControl>
         </div>
         <div>
-          <div>属性</div>
-          {
-            attributeButtons.map(data => <label key={`attribute_${data.value}`}>
-              <input type="radio" name="attribute" value={data.value} checked={attribute === data.value} onChange={() => setAttribute(data.value)}/>
-              {data.label}
-            </label>)
-          }
+          <FormControl component="fieldset">
+            <FormLabel component="legend">属性</FormLabel>
+            <RadioGroup aria-label="attribute" name="attribute" value={attribute} onChange={e => setAttribute(e.target.value)}>
+            {
+              attributeButtons.map(data => <FormControlLabel key={`attribute${data.value}`} value={data.value} control={<Radio/>} label={data.label}/>)
+            }
+            </RadioGroup>
+          </FormControl>
         </div>
         <div>
-          <div>名前</div>
-          <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+          <div><TextField label="名前" value={name} onChange={e => setName(e.target.value)}/></div>
+          <div><TextField label="肩書き" value={title} onChange={e => setTitle(e.target.value)}/></div>
+          <div><TextField label="能力" value={ability} onChange={e => setAbility(e.target.value)}/></div>
+          <div><TextField label="能力の説明" value={abilityNote} onChange={e => setAbilityNote(e.target.value)}/></div>
+          <div><TextField multiline={true} label="説明" value={description} onChange={e => setDescription(e.target.value)}/></div>
+          <div><TextField label="QRコードを作りたい場合はテキスト入れてね" value={qrText} onChange={e => setQrText(e.target.value)}/></div>
         </div>
         <div>
-          <div>肩書き</div>
-          <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+          <div><Button onClick={generate} variant="contained" color="primary">カード画像作成</Button></div>
+          {cardData && <div><img src={cardData}/></div>}
         </div>
-        <div>
-          <div>能力</div>
-          <input type="text" value={ability} onChange={e => setAbility(e.target.value)}/>
-        </div>
-        <div>
-          <div>能力の説明</div>
-          <input type="text" value={abilityNote} onChange={e => setAbilityNote(e.target.value)}/>
-        </div>
-        <div>
-          <div>説明</div>
-          <textarea value={description} onChange={e => setDescription(e.target.value)}></textarea>
-        </div>
-        <div>
-          <button type="button" onClick={download}>ダウンロード</button>
-        </div>
-      </form>
-    </div>
-  </>;
+      </Grid>
+    </Grid>
+  </Container>;
 };
 
 export default App;
